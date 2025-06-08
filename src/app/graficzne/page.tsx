c947e683fbd5407e4f6d0c7259e644c24c8b5678
Stanislaw Blicharski <mainly.agn@gmail.com>
2025-06-08 13:31:32 +0200

    poprawki eslink

--- src/app/graficzne/page.tsx
+++ src/app/graficzne/page.tsx
@@ -18,7 +18,7 @@ export default function GraficznePage() {
             <Home className="w-5 h-5" />
           </SidebarMenuItem>
           <SidebarMenuItem label="Graficzne" href="/graficzne">
-            <Image className="w-5 h-5" />
+            <Image className="w-5 h-5" alt="Ikona graficzna" />
           </SidebarMenuItem>
           <SidebarMenuItem label="Ustawienia" href="#">
             <Settings className="w-5 h-5" />
--- src/app/prompt/[id]/page.tsx
+++ src/app/prompt/[id]/page.tsx
@@ -159,7 +159,12 @@ const similarPrompts = [
   },
 ];
 
-export default function PromptPage({ params }: { params: { id: string } }) {
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export default async function PromptPage({ params }: Props) {
   const prompt = prompts.find((p) => p.id === params.id);
 
   if (!prompt) {
